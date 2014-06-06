b2b207a95283e50358871add9f63f678ce637fec
Kristiyan <zontak@mail.bg>
2014-06-06 13:20:33 +0300

    Sum Two Numbers
    Sum Two Numbers

---
+++ FirstHomework/SumTwoNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.printf("Enter i Value:  ");
+	    int i = in.nextInt();
+	    System.out.printf("Enter j Value:  ");
+	    int j = in.nextInt();
+	    int sum = i + j ;
+	    System.out.println(sum);
+	}
+
+}
