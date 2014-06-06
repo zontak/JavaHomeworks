a321f767ea1ac67e890728fd0cad8b3367b3d012
Kristiyan <zontak@mail.bg>
2014-06-06 13:25:48 +0300

    RUN CMD

---
+++ FirstHomework/run.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+ECHO You started a program that enters from the console n count of strings, then sorts them alphabetically and prints them!
+ECHO[
+CHCP 1251
+java -jar "C:\workspace\Intro-Java-Homework\PrintHome.jar"
+PAUSE
